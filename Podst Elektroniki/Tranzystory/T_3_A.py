80d77b2d2ec4c838ad081f44b6bcd3807ac3a154
pawello85 <1zabelpawel@gmail.com>
2025-06-04 10:18:51 +0200

    T_3_A finish

--- Podst Elektroniki/Tranzystory/T_3_A.py
+++ Podst Elektroniki/Tranzystory/T_3_A.py
@@ -1,30 +1,68 @@
-import math
+# import math
+# Dane
+print("Dane:")
 
 Beta = float(input("Beta = "))
-print("Beta =", Beta)
+I_b = float(input("I_b [mA] = "))
+U_cc = float(input("U_cc [V] = "))
+R_c = float(input("R_c [Ohm] = "))
+gm = float(input("gm [S] = "))
+r_ce = float(input("r_ce [Ohm] = "))
+A = float(input("A [V] = "))
+R_o = float(input("R_o [Ohm] = "))
+R_g = float(input("R_g [Ohm] = "))
 
-I_b = float(input("I_b = "))
-print("I_b =", I_b,"[mA]")
 
+print("\n---\nWyniki:")
+
+# 1)
 I_cx = Beta * I_b
-print(f"---\n{I_cx}[mA]\n---")
-
-U_cc = float(input("U_cc = "))
-print("U_cc =", U_cc,"[V]")
-
-R_c = float(input("R_c = "))
-print("R_c =", R_c,"[Ohm]")
+print("1)\nI_cx = Beta * I_b\nI_cx =",I_cx,"[mA]\n")
 
 U_ce = U_cc - R_c * I_cx / 1000
-print("---\nU_ce = U_cc - R_c * I_cx =",U_ce, "[V]\n---")
+print("U_ce = U_cc - R_c * I_cx \nU_ce =",U_ce, "[V]")
 
-print("2)")
-gm =float(input("gm ="))
-r_ce = float(input("r_ce ="))
-A = float(input("A ="))
-r_wy = R_c * r_ce /(R_c + r_ce)
-k_uo = round(- gm * r_wy,0)
-print(k_uo)
+
+# 2) Amplituda dla biegu jaowego
+
+print("\n2)")
+
+r_wy = round(R_c * r_ce /(R_c + r_ce),2)
+
+print("r_wy = R_c * r_ce /(R_c + r_ce)\nr_wy =", r_wy,"[Ohm]\n")
+k_uo = round(-gm * r_wy,0)
+print("k_uo = -gm * r_wy \nk_uo=", k_uo)
+
 B = abs(k_uo * A)
-print(B)
-print("3)")
+print("B =", B, "V")
+
+
+# 3) 
+
+print("\n3)")
+
+
+
+k_u = round(k_uo * R_o /(R_o + r_wy),0)
+B = abs(k_u * A)
+print("k_u = ", k_u)
+print("B =", B, "V")
+
+
+# 4)
+
+print("\n4)")
+R_BE = Beta/gm
+
+k_uc = round(k_u * R_BE/ (R_BE + R_g),0)
+B = abs(k_uc * A)
+
+print("k_uc = ", k_uc)
+print("B =", B, "V\n---")
+
+
+
+
+
+
+
